962213fcd1f56ddda2c6efe96f0cce087cda346b
pgtoopx <eduardoinbrand@gmail.com>
2019-05-31 15:20:21 -0400

    initial

---
+++ src/test/java/driver/util.java
@@ -0,0 +1,16 @@
+package driver;
+
+import cucumber.api.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+
+public class util {
+    public static void printCurrentThread(){
+        System.out.println("Current Thread : " + Thread.currentThread().getId());
+    }
+
+    public static void takeAndEmbedScreenshot(Scenario scenario){
+        byte[] screenshot = ((TakesScreenshot)DriverFactory.getDriver()).getScreenshotAs(OutputType.BYTES);
+        scenario.embed(screenshot, "image/png");
+    }
+}
---
+++ src/test/java/runner/Runner.java
@@ -0,0 +1,13 @@
+package runner;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(glue = "stepdef", features = "src/test/resources/features/",
+plugin = {"pretty","json:target/cucumber-report/cucumber.json", "junit:target/cucumber.xml", "html:target/cucumber-report"})
+public class Runner {
+
+}
