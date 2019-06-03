e8082e511dc27dc27bb111a4ba7eed85b45099b0
pgtoopx <eduardoinbrand@gmail.com>
2019-06-03 16:11:30 -0400

    add some utils

--- src/test/java/driver/util.java
+++ src/test/java/driver/util.java
@@ -1,8 +1,7 @@
 package driver;
 
 import cucumber.api.Scenario;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.*;
 
 public class util {
     public static void printCurrentThread(){
@@ -12,5 +11,41 @@ public class util {
     public static void takeAndEmbedScreenshot(Scenario scenario){
         byte[] screenshot = ((TakesScreenshot)DriverFactory.getDriver()).getScreenshotAs(OutputType.BYTES);
         scenario.embed(screenshot, "image/png");
+    }
+
+    public static void waitFor(int segundos){
+        try {
+            Thread.sleep(segundos*1000);
+        }catch (InterruptedException e){
+            System.out.println("error de Thread.sleep");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static boolean isVisible(By by){
+        Boolean isVisible =  true; //asume que la pagina esta cargando
+        try {
+            isVisible = DriverFactory.getDriver().findElement(by).isDisplayed(); // si es true es decir que esta cargando
+        }catch (NoSuchElementException e){
+            isVisible=false; // si entra al catch quiere decir que no encontro el elemento, es decir ya cargo
+        }
+        return isVisible;
+    }
+
+
+    public static boolean isVisible(WebElement webElement){
+        Boolean isVisible =  true; //asume que la pagina esta cargando
+        try {
+            isVisible = webElement.isDisplayed(); // si es true es decir que esta cargando
+        }catch (NoSuchElementException e){
+            isVisible=false; // si entra al catch quiere decir que no encontro el elemento, es decir ya cargo
+        }
+        return isVisible;
+    }
+
+    public static void waitUntilEscritorioComercialIsLoaded(){
+        while (util.isVisible(By.id("loading-bar"))) {
+            util.waitFor(1);
+        }
     }
 }
